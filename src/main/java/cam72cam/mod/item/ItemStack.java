ccc04c96f8d0b389e68ebf67feddb488afad79dd
Appw <austinpaulpal@gmail.com>
2021-05-12 23:03:21 -0700

    Hacky Use Item Fix
    A hacky fix to allow some items to be used while riding a train

--- src/main/java/cam72cam/mod/item/ItemStack.java
+++ src/main/java/cam72cam/mod/item/ItemStack.java
@@ -1,9 +1,12 @@
 package cam72cam.mod.item;
 
 import cam72cam.mod.entity.Player;
+import cam72cam.mod.entity.Player.Hand;
 import cam72cam.mod.serialization.TagCompound;
+import cam72cam.mod.world.World;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraft.util.ActionResult;
 import net.minecraftforge.fluids.FluidUtil;
 
 /** Wrapper around Minecraft ItemStack (Item, count, NBT) */
@@ -137,5 +140,9 @@ public class ItemStack {
     /** Completely null out the tag compound */
     public void clearTagCompound() {
         internal.setTagCompound(null);
+    }
+
+    public void use(World world, Player player, Hand hand) {
+        internal.useItemRightClick(world.internal, player.internal, hand.internal);
     }
 }
